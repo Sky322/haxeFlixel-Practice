32a9aa992ad9565189b0d90c2e1a8f06880b1507
Sky322 <sky322@live.missouristate.edu>
2020-04-05 13:37:54 -0500

    created FlxText object

---
+++ source/PlayState.hx
@@ -0,0 +1,18 @@
+package;
+
+import flixel.FlxState;
+import flixel.text.FlxText;
+
+class PlayState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+		new FlxText(0,0,0,"Hello World", 32);
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
